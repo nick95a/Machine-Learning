93d058726dfa774b29f7f62fc3fa92855aa92498
Nikolai Maltcev <nick95a@yandex.ru>
2019-07-29 10:53:42 +0500

    Practicing different classification techniques on the breast cancer dataset

--- breast-cancer/breast_cancer.py
+++ breast-cancer/breast_cancer.py
@@ -1,10 +1,25 @@
 from sklearn import datasets
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
+
+# Step 1.
 # Load the dataset
 dataset = datasets.load_breast_cancer()
 X = pd.DataFrame(dataset.data, columns = dataset.feature_names)
 y = pd.DataFrame(dataset.target)
+
+assert X.isnull().sum().sum() == 0
+assert y.isnull().sum().sum() == 0
+
+assert X.duplicated().sum().sum() == 0
+
+# Summary statistics for the data, checking dtypes for our data and seeing the shape of our data
+X.describe()
+X.info()
+# Hence, no duplicates and no NaN values. All of the variables are float64 type, so no problems here
+
+
+max_values = np.transpose(X.describe()[-1:][:])
