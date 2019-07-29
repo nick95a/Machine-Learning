9471d8b306ea0b117e79a42a1a77b9d8c189b5f7
Nikolai Maltcev <nick95a@yandex.ru>
2019-07-29 11:46:39 +0500

    Practicing different classification techniques on the breast cancer dataset

--- breast-cancer/breast_cancer.py
+++ breast-cancer/breast_cancer.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 dataset = datasets.load_breast_cancer()
 X = pd.DataFrame(dataset.data, columns = dataset.feature_names)
 y = pd.DataFrame(dataset.target)
+column_names = list(dataset.feature_names)
+print(column_names)
 
 assert X.isnull().sum().sum() == 0
 assert y.isnull().sum().sum() == 0
@@ -17,9 +19,25 @@ assert y.isnull().sum().sum() == 0
 assert X.duplicated().sum().sum() == 0
 
 # Summary statistics for the data, checking dtypes for our data and seeing the shape of our data
-X.describe()
-X.info()
+
+#X.describe()
+#X.info()
+
 # Hence, no duplicates and no NaN values. All of the variables are float64 type, so no problems here
+def plotter(column_names, X):
+    fig = plt.figure(figsize=(10, 10))
+    fig.subplots_adjust(hspace = 1.1,wspace = 0.4)
+    for index, name in enumerate(column_names):
+        ax = fig.add_subplot(10, 3, index + 1)
+        ax.hist(X.iloc[:, index])
+        ax.set_title(label = name)
+    plt.show()
+
+#plotter(column_names, X)
 
 
+
+
+
+# Scaling the data
 max_values = np.transpose(X.describe()[-1:][:])
