26686abfc0f60e3a9a932e78ffcb4ba3d8de69c0
Nikolai Maltcev <nick95a@yandex.ru>
2019-08-01 11:53:45 +0500

    Practicing different classification techniques on the breast cancer dataset

--- Digit_classification.py
+++ Digit_classification.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 dataset = datasets.load_digits()
 
@@ -14,3 +16,19 @@ X.shape
 new_dataset = np.array(dataset.images)
 new_dataset = np.reshape(new_dataset, newshape = (new_dataset.shape[0], -1))
 new_dataset.shape
+
+y_series = y.iloc[:,0]
+y_series.value_counts()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
+
+# visualise the dataset of images
+
+images = list(zip(dataset.images, dataset.target))
+for index, (image, label) in enumerate(images[:4], 1):
+    plt.subplot(2, 4, index)
+    plt.axis('off')
+    plt.imshow(image, cmap = "Greys")
+    plt.title("Training image for number {}".format(label))
+
+plt.show()
