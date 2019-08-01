2027614609d79b9c756d269a98d7b4f4dffbe51a
Nikolai Maltcev <nick95a@yandex.ru>
2019-08-01 14:04:34 +0500

    Practicing different classification techniques on the breast cancer dataset

--- Digit_classification.py
+++ Digit_classification.py
@@ -3,14 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import classification_report, roc_auc_score, roc_curve, auc, accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
 
 dataset = datasets.load_digits()
 
 X = pd.DataFrame(dataset.data)
 y = pd.DataFrame(dataset.target)
+y = y.iloc[:, 0]
 
 X.shape
 # We see that there is no need to transform the data from the original shape. Normally, when processing
@@ -19,11 +22,8 @@ new_dataset = np.array(dataset.images)
 new_dataset = np.reshape(new_dataset, newshape = (new_dataset.shape[0], -1))
 new_dataset.shape
 
-y = y.iloc[:,0]
-y.value_counts()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
-
 # visualise the dataset of images
 
 images = list(zip(dataset.images, dataset.target))
@@ -33,10 +33,27 @@ for index, (image, label) in enumerate(images[:4], 1):
     plt.imshow(image, cmap = "Greys")
     plt.title("Training image for number {}".format(label))
 
-params = {'C': np.linspace(0.1,2,20), 'kernel' : ['rbf', 'linear', 'rbf','sigmoid']}
-classifier = SVC()
+params = {'C': np.linspace(0.1,2,10), 'kernel' : ['rbf', 'linear','sigmoid']}
+
+# gamma is small since we do not need the classifier to overfit.
+classifier = SVC(gamma = 0.0001)
 classifier = GridSearchCV(classifier, param_grid = params,cv = 5)
 classifier.fit(X_train, y_train)
 predictions = classifier.predict(X_test)
-probas = classifier.predict_proba(X_test)
-roc_auc_score(y_test, probas)
+clf_report = classification_report(y_test, predictions)
+accuracy = accuracy_score(y_test, predictions)
+print(clf_report, accuracy)
+
+# TO-DO: ROC-AUC for this problem, plot them.
+"""
+To plot roc_auc for multuclass classification one needs to binarize the variables first.
+Then use the OneVsRestClassifier
+y = label_binarize(y, classes = list(range(10)))
+
+fpr = {}
+tpr = {}
+roc_auc = {}
+for i in range(10):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], probas[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+"""
