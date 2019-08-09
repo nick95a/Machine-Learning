8c5427b8324ac53fd7f7f02c01dc017b30332a16
Nikolai Maltcev <nick95a@yandex.ru>
2019-08-09 21:47:45 +0500

    Practicing different classification techniques on the breast cancer dataset

--- Text_classification.py
+++ Text_classification.py
@@ -3,11 +3,11 @@ import numpy as np
 import pprint
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.datasets import fetch_20newsgroups
 
 # Load the dataset and the names of the categories
-news_bunch = fetch_20newsgroups(subset = 'all', shuffle = True, random_state = 24)
+news_bunch = fetch_20newsgroups(subset = 'train', shuffle = True, random_state = 24)
 categories = news_bunch.target_names
 dataset = pd.DataFrame(news_bunch.data)
 
@@ -21,10 +21,21 @@ news = s.split('\n')
 for n in news:
     print(n)
 '''
-# Get the occurencies of the words in the whole corpus of documents
+# Get the occurencies of the words in the whole corpus of documents.
+# Here we use a combination of CountVectorizer and TfidfTransformer while the TfidfVectorizer does both
 vect = CountVectorizer()
-X = vect.fit_transform(news_bunch.data)
-X.shape
+X_train = vect.fit_transform(news_bunch.data)
+X_train.shape
 stop_words, vocab = vect.stop_words_, vect.vocabulary_
 
 # Bring meaning to the counts by finding term frequencies relative to the size of the whole corpus
+trans = TfidfTransformer()
+X_train_new = trans.fit_transform(X_train)
+
+# idf attribute show the inverse-document frequency weightings: one for each feature
+trans.idf_
+
+# Check out the shape of the transformed feature map
+X_train_new.shape
+
+
