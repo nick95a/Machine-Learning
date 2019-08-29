ade0613bed88e01080b000a5f5270b923309f71c
Nikolai Maltcev <nick95a@yandex.ru>
2019-08-29 18:37:41 +0500

    Practicing different classification techniques on the breast cancer dataset

--- Gradient Boosting.py
+++ Gradient Boosting.py
@@ -40,8 +40,8 @@ reg_GB = GradientBoostingRegressor(random_state = 24)
 gb_reg = GridSearchCV(reg_GB, gb_params, cv = 5)
 gb_reg.fit(X_train, y_train)
 
-tree_reg = DecisionTreeRegressor()
-ada_reg = AdaBoostRegressor(tree_reg, random_state = 24)
+tree_reg = DecisionTreeRegressor(max_depth = 4)
+ada_reg = AdaBoostRegressor(tree_reg, random_state = 24, n_estimators = 200)
 ada_reg.fit(X_train, y_train)
 
 
@@ -54,8 +54,8 @@ def compare_models(models : dict, X_test, y_test):
         mse = mean_squared_error(y_test, y_pred)
         median_ae = median_absolute_error(y_test, y_pred)
         mean_ae = mean_absolute_error(y_test, y_pred)
-        r2_score = r2_score(y_test, y_pred)
-        results[name] = [explained_variance, mse, median_ae, mean_ae, r2_score]
+        r2 = r2_score(y_test, y_pred)
+        results[name] = [explained_variance, mse, median_ae, mean_ae, r2]
     return results
 
 results = compare_models(models, X_test, y_test)
